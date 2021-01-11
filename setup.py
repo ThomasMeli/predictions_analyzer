ddfdba74efab42ef148a2b21059b972cda9cbf6a
Thomas Meli <tpmeli@gmail.com>
2021-01-11 10:55:13 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="predictions-analyzer-tpmeli",
-    version="0.0.21",
+    name="predictions_analyzer",
+    version="0.0.22",
     author="Thomas Meli",
     author_email="tpmeli.data@gmail.com",
     description="A powerful tool for analyzing and ensembling predictions",
